c59d821d2ce17ff84c51ca09f85223e0f85879bc
Andrew Bloyce <bloyce1990@gmail.com>
2020-03-21 23:37:47 +1000

    CSS

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="container text-center">
+        <p>
+          <a
+            href="https://github.com/bloycey/google-sheets-text-adventure"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Make your own text adventure!
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/components/StoryPage.js
+++ src/components/StoryPage.js
@@ -1,45 +1,46 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
 
+import Footer from "./Footer";
+
 const formatCommaSeperatedData = arrayOfData =>
-	arrayOfData
-		.split(",")
-		.map(choice => choice.trim())
-		.filter(choice => choice !== "");
+  arrayOfData
+    .split(",")
+    .map(choice => choice.trim())
+    .filter(choice => choice !== "");
 
 const StoryPage = ({ storyData }) => {
-	const { id } = useParams();
-	const correctPageData = storyData.find(story => story.url === id);
-	if (!correctPageData || correctPageData.length < 1) {
-		return (
-			<div className="container">
-				<p>This doesn't look right...</p>
-				<Link to="/" className="link-btn">
-					Return to home
-				</Link>
-			</div>
-		);
-	}
-	const { title, content, choices, image, paths } = correctPageData;
-	const formattedChoices = formatCommaSeperatedData(choices);
-	const formattedPaths = formatCommaSeperatedData(paths);
-	return (
-		<div className="container">
-			{title && <h1>{title}</h1>}
-			{content && <p className="content">{content}</p>}
-			{image && <img src={image} alt="" />}
-			{formattedChoices.length > 0 &&
-				formattedChoices.map((choice, index) => (
-					<Link
-						to={formattedPaths[index]}
-						className="link-btn"
-						key={choice}
-					>
-						{choice}
-					</Link>
-				))}
-		</div>
-	);
+  const { id } = useParams();
+  const correctPageData = storyData.find(story => story.url === id);
+  if (!correctPageData || correctPageData.length < 1) {
+    return (
+      <div className="container">
+        <p>This doesn't look right...</p>
+        <Link to="/" className="link-btn">
+          Return to home
+        </Link>
+      </div>
+    );
+  }
+  const { title, content, choices, image, paths } = correctPageData;
+  const formattedChoices = formatCommaSeperatedData(choices);
+  const formattedPaths = formatCommaSeperatedData(paths);
+  return (
+    <>
+      <div className="main container">
+        {title && <h1>{title}</h1>}
+        {content && <p className="content">{content}</p>}
+        {image && <img src={image} alt="" />}
+        {formattedChoices.length > 0 &&
+          formattedChoices.map((choice, index) => (
+            <Link to={formattedPaths[index]} className="link-btn" key={choice}>
+              {choice}
+            </Link>
+          ))}
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default StoryPage;
